eb2224fe3a5c1ec92ff66cfe48773aa6771ea986
Diogo Ewerton <diogoesn1212@gmail.com>
2025-04-09 20:47:06 -0300

    Add files via upload

---
+++ exercicio14.py
@@ -0,0 +1,14 @@
+import time
+
+for i in range(1000, 2001):
+    if i % 11 == 5:
+        print(f'o numero é {i}')
+        time.sleep(1)
+
+numero = 1000
+
+    while numero <= 2000:
+        if numero % 11 == 5:
+            print(f'o número é {numero}')
+            
+
