c618817adddfb420abcd8cfcd16f7cd29c6ac941
Diogo Ewerton <diogoesn1212@gmail.com>
2025-04-09 21:27:16 -0300

    Add files via upload

---
+++ exercicio16.py
@@ -0,0 +1,4 @@
+nomes_amigos = ['Keivison', 'Laiane', 'Rafaella']
+
+for nome in nomes_amigos:
+    print(nome)
---
+++ exercicio17.py
@@ -0,0 +1,4 @@
+nomes_amigos = ['Keivison', 'Laiane', 'Rafaella']
+
+for nome in nomes_amigos:
+    print(f'Olá {nome}, como você está? ')
